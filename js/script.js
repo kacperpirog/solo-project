10b16bd48934eb2b95dbc61af6a586843df06d9d
kacper pirog <kacper.pirog@gmail.com>
2020-11-27 15:19:03 +0100

    add nav

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const navList = document.querySelector('nav__list__wrapper');
+
+class Navigation { 
+    constructor(navBarList)
+
+
+}
+
+createNav();{
+    const navBar = document.createElement('li');
+    listItem.classList.add('nav__bar');
+
+};
