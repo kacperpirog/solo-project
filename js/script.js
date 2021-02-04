ab80608cc10130d799477615d3fb455bd73a15eb
kacper pirog <kacper.pirog@gmail.com>
2021-02-04 10:26:31 +0100

    add page _general & modal

--- js/script.js
+++ js/script.js
@@ -1,13 +1,98 @@
-const navList = document.querySelector('nav__list__wrapper');
-
-class Navigation { 
-    constructor(navBarList)
 
 
+
+
+$(window).on('load resize', function () {
+    if ($(this).width() < 640) {
+      $('table tfoot').hide();
+    } else {
+      $('table tfoot').show();
+    }  
+  });
+
+
+
+  
+  function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+  }
+  document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault()
+      closeModal()
+    })
+  })
+
+  document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+      closeModal()
+    }
+  })
+
+  document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+      closeModal()
+    }
+  })
+
+  function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove('show')
+    })
+    document.querySelector('#overlay').classList.add('show')
+    document.querySelector(modal).classList.add('show')
+  }
+const modal = document.querySelector("#myModal")
+
+const showModalBtn = document.querySelector(".showModal__btn")
+const overlay = document.querySelector(".overlay")
+const hideModalBtn = document.querySelector(".btn-close")
+
+
+const handleModalOpen = () => {
+  overlay.style.display = "flex"
+  openModal('#myModal')
+  console.log(overlay);
 }
 
-createNav();{
-    const navBar = document.createElement('li');
-    listItem.classList.add('nav__bar');
+const handleModalClose = () => {
+  overlay.style.display = "none"
+  openModal('#myModal')
+}
+showModalBtn.addEventListener("click", handleModalOpen)
+hideModalBtn.addEventListener("click", handleModalClose)
 
-};
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var chart = new Chart(ctx, {
+    // 1
+    type: 'bar',
+    data: {
+        // 2
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+        // 3
+        datasets: [{
+            // 4
+            label: "Signups",
+            // 5
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
+            // 6
+            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+            // 7
+            hidden: true,
+        }]
+    },
+});
